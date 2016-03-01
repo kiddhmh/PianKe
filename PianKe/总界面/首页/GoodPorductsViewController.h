d798441663093466a850c3642f896ffb708aa3d4
kiddhmh <hmh7747952@126.com>
2016-03-01 23:26:28 +0800

    片刻登录注册功能完善
    功能完善

---
+++ PianKe/总界面/首页/GoodPorductsViewController.h
@@ -0,0 +1,14 @@
+//
+//  GoodPorductsViewController.h
+//  PianKe
+//
+//  Created by 胡明昊 on 16/2/25.
+//  Copyright © 2016年 CMCC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoodPorductsViewController : UIViewController
+
+
+@end
---
+++ PianKe/总界面/首页/Model/Carousel.m
@@ -0,0 +1,13 @@
+//
+//  Carousel.m
+//  PianKe
+//
+//  Created by 胡明昊 on 16/2/25.
+//  Copyright © 2016年 CMCC. All rights reserved.
+//
+
+#import "Carousel.h"
+
+@implementation Carousel
+
+@end
